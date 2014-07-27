5628775b59ab84bcd0449b6896be4c161899df42
job.louage@gmail.com <job.louage@gmail.com>
2014-07-27 20:58:07 +0200

    Update SERVICE_URL

--- CarpoolBijoux/src/net/louage/bijoux/constants/Constants.java
+++ CarpoolBijoux/src/net/louage/bijoux/constants/Constants.java
@@ -5,7 +5,6 @@ public class Constants {
 	//public static String SERVICE_URL="ftp://louagej@ftp.louage.net/public_html/cgi-bin/mysql/db_users.php";
 	//public static String SERVICE_URL="http://louage.net/cgi-bin/mysql/db_users.php";	
 	public static String SERVICE_URL="http://louage.net/db_login.php";
-	public static String MY_TOURS_URL="http://www.hitektrackingjob.net84.net/db_my_tours.php";
 	
 	//Variable to store the User ID of the current user
 	public static int USER_ID;
